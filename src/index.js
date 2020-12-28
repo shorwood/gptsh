b4ee7dbf274b36672534aac2e579e52479d83540
Stanley Horwood <stanley@hsjm.io>
2020-12-28 22:03:03 +0000

    cleaned up usage

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@
 
 //--- Import dependencies.
 const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const uniq = require('lodash/uniq')
 const data = require('../data')
 const complete = require('./complete')
 const buildConfig = require('./buildConfig')
 
 //--- Parse argv arguments using 'yargs' package.
-const argv = yargs(process.argv.slice(2))
+const argv = yargs(hideBin(process.argv))
 	.usage('Usage: $0 <query> [options]')
-	.usage('Example: $0 "List all files of this directory" | bash')
-	.usage('Example: $0 "Install the lodash package" --secret <YOUR_SECRET_KEY> | bash')
-	.usage('Example: $0 "Delete the root directory" --engine ada')
-	.usage('Example: $0 "Merge the staing branch into master" --max-tokens 32')
-	.option('engine', {type: 'string', alias: 'e', description: 'The ID of the engine to use for this request'})
-	.option('secret', {type: 'string', alias: 's', description: 'The OpenAI API API keys for authentication'})
+	.example('$0 List all files of this directory | bash')
+	.example('$0 Install the lodash package --secret <YOUR_SECRET_KEY> | bash')
+	.example('$0 Delete the root directory --engine ada')
+	.example('$0 Merge the staing branch into master --max-tokens 32')
+	.option('engine', {type: 'string', alias: 'e', description: 'The ID of the engine to use'})
+	.option('secret', {type: 'string', alias: 's', description: 'The OpenAI API key for authentication'})
 	.option('max-tokens', {type: 'number', alias: 't', description: 'The maximum number of tokens to generate'})
-	.option('n', {type: 'number', description: 'How many completions to generate for each prompt.'})
+	.option('n', {type: 'number', description: 'How many completions to generate for each prompt'})
+	.demandCommand(1)
 	.help()
 	.argv
 
