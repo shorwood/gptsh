c9dc8671dddf3438ac452acb7ea064c5585c54ee
Stanley Horwood <stanley@hsjm.io>
2020-12-29 01:30:12 +0000

    renamed variables

--- src/complete.js
+++ src/complete.js
@@ -10,12 +10,12 @@ const openai = axios.create({
 	headers: {'Content-Type': 'application/json'}
 })
 
-module.exports = async (prompt, options = {}) => {
+module.exports = async (prompt, config = {}) => {
 
 	//--- Parse options.
-	const secret = options.secret
-	const engineId = options.engineId
-	const payload = pick(options, [
+	const secret = config.secret
+	const engineId = config.engineId
+	const options = pick(config, [
 		'max_tokens', 'temperature', 'top_p',
 		'n', 'stream', 'logprobs', 'echo',
 		'stop', 'presence_penalty', 'frequency_penalty',
@@ -26,7 +26,7 @@ module.exports = async (prompt, options = {}) => {
 	openai.defaults.headers.common['Authorization'] = `Bearer ${secret}`
 
 	//--- Query the GPT-3 API.
-	return await openai.post(`/engines/${engineId}/completions`, {prompt, ...payload})
+	return await openai.post(`/engines/${engineId}/completions`, {prompt, ...options})
 		.then(res => map(res.data.choices, 'text'))
 		.catch(err => console.error(err.response.data.error.message || err.message))
 }
